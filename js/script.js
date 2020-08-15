b436f17d1cbb72c16e387fe0dfdcdd7244c2f895
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-08-15 18:50:31 -0400

    Projeto_Site pessoal Finalizado

--- js/script.js
+++ js/script.js
@@ -2,17 +2,25 @@
 
 const modalOverlay = window.document.querySelector('.modal-overlay')
 const cards = window.document.querySelectorAll('.card')
-
+/*
+console.log(cards.length)
+console.log(cards);
+console.log(cards[0])*/
 
 
 for (const card of cards) {
-    card.addEventListener('click',function () {
+    card.addEventListener('click', function () {
+        const videoId = card.getAttribute('id')
         modalOverlay.classList.add('active') /*o metodo 'classList é um metodo para adicionar classes*/
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
+
     })
 }
 
 
-// FecharModa
-window.document.querySelector('.close-modal').addEventListener('click',function () {
+// FecharModalOverlay
+window.document.querySelector('.close-modal').addEventListener('click', function () {
     modalOverlay.classList.remove('active') // remove a classe active
+    modalOverlay.querySelector('iframe').src = "" // ao clicar tira a url e para o video
 })
+
